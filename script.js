3e899c52cd1f9166a058ddede0430eafe99e02ae
Annaa7L <lebedevaa471@gmail.com>
2024-11-26 11:44:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let menu = document.querySelector(".menu");
+let menuToggle = document.querySelector(".menu-toggle");
+menuToggle.addEventListener("click", function(e){
+    e.preventDefault();
+    menu.classList.toggle("show-menu");
+    menuToggle.classList.toggle("active");
+})
+window.addEventListener('resize', function(){
+    if(this.window.innerWidth > 375 && menu.classList.contains('show-menu')){
+        menu.classList.remove('show-menu');
+        menuToggle.classList.remove("active");
+    }
+})
